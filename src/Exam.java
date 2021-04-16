cf228633e9160c1a5c0bf6d4f25acfc2926bb3ab
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-04-16 15:00:44 +0200

    Added to Class Exam

--- src/Exam.java
+++ src/Exam.java
@@ -1,4 +1,22 @@
 public class Exam {
+
+  //ATTRIBUTES
   private String name;
   private int grade;
+
+  //CONSTRUCTOR
+  public Exam(String name, int grade){
+    this.name = name;
+    setGrade(grade);
+  }
+
+  //GETTER
+  public int getGrade(){
+    return grade;
+  }
+
+  //SETTER
+  public void setGrade(int grade){
+    this.grade = grade;
+  }
 }
