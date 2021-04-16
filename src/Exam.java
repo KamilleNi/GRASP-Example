cb3a37bee6210c463d4dba344e30de7005b93358
Michala Nybroe <michalanim@gmail.com>
2021-04-16 15:00:07 +0200

    Student bare info

--- src/Exam.java
+++ src/Exam.java
@@ -1,4 +1,6 @@
 public class Exam {
   private String name;
   private int grade;
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,22 @@
 public class Student {
   private String name;
-  private String ID;
-  Exam[] exams;
+  private String iD;
+  Exam[] exams = new Exam[7];
 
+  public Student(String name, String iD) {
+    setName(name);
+    this.iD = iD;
+  }
+
+  public void setExams(Exam[] exams) {
+    this.exams = exams;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Exam[] getExams() {
+    return exams;
+  }
 }
