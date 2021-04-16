86a5df033f2e901e30563d8ffd2b0397c7adb9df
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-04-16 17:04:17 +0200

    Deleted method call in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
   public static void main(String[] args){
-<<<<<<< HEAD
-    School kea = new School();
-
-    kea.runMenu();
-=======
 
     School KEA = new School();
     KEA.runMenu();
->>>>>>> 1fb9b0d98b604abb9696d1283bc85b873061d5bd
   }
 }
